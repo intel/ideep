7f6d5bcfbfb084a8640dda4422feada23bbbc767
caozhong <zhong.z.cao@intel.com>
2017-10-08 16:55:34 +0800

    Adjust file name to be more verbose
    Signed-off-by: caozhong <zhong.z.cao@intel.com>

--- ideep/__init__.py
+++ ideep/__init__.py
@@ -4,11 +4,10 @@ import ctypes  # NOQA
 # For C++ extension to work
 sys.setdlopenflags(sys.getdlopenflags() | ctypes.RTLD_GLOBAL)
 
-# API lift
 from ideep import api  # NOQA
-from ideep import chainer  # NOQA
+from ideep import xnn  # NOQA
 from ideep import compute_complex  # NOQA
 
 from ideep.mdarray import mdarray  # NOQA
 
-from ideep.chainer.fanout import FanoutRecorder  # NOQA
+from ideep.xnn.fanout import FanoutRecorder  # NOQA

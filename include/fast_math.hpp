200b6b2efe77dd1189402176273499e48ac873bd
caozhong <zhong.z.cao@intel.com>
2018-01-19 14:54:25 +0800

    Possible multithread version and unified interface
    Signed-off-by: caozhong <zhong.z.cao@intel.com>

--- include/fast_math.hpp
+++ include/fast_math.hpp
@@ -1,8 +1,10 @@
 #ifndef _FAST_MATH_HPP_
 #define _FAST_MATH_HPP_
 #include <assert.h>
+#include <string>
 #include <type_traits>
 #include <immintrin.h>
+#include <ideep/abstract_types.hpp>
 namespace ideep {
 namespace utils {
 
@@ -18,6 +20,7 @@ typedef enum {
 
 template<cpu_isa_t isa = avx2>
 class fast_math {
+  static constexpr int thread_hold = 1024;
 public:
   // Move this to utils
   static inline __m256i size_to_mask(unsigned nres) {
@@ -44,13 +47,13 @@ public:
     }
   }
 
-  template<typename T = float, typename vec_op, typename vec_op_mask>
+  template<typename vec_op, typename vec_op_mask, typename T = float>
   static inline void single_thread_vecwise_unary_op(
       T *dst, const T *src, size_t nelems,
       vec_op op, vec_op_mask op_mask) {
     constexpr unsigned vec_sz = 256/8/sizeof(float);
-    size_t nvec = nelems / vec_sz;
-    size_t nres = nelems % vec_sz;
+    auto nvec = nelems / vec_sz;
+    auto nres = nelems % vec_sz;
     for (unsigned vec = 0; vec < nvec; vec ++, src+=vec_sz, dst+=vec_sz) {
       __m256 vmm1 = _mm256_load_ps(src);
       vmm1 = op(vmm1);
@@ -65,13 +68,20 @@ public:
     }
   }
 
-  template<typename T = float, typename vec_op, typename vec_op_mask>
+  template<typename vec_op, typename vec_op_mask, typename T = float>
+  static inline void vecwise_unary_op (T *dst, const T *src, size_t nelems,
+      vec_op op, vec_op_mask op_mask) {
+    if (nelems < thread_hold)
+      single_thread_vecwise_unary_op(dst, src, nelems, op, op_mask);
+  }
+
+  template<typename vec_op, typename vec_op_mask, typename T = float>
   static inline void single_thread_vecwise_binary_op(
       T *dst, const T *src, size_t nelems,
       vec_op op, vec_op_mask op_mask) {
     constexpr unsigned vec_sz = 256/8/sizeof(float);
-    unsigned nvec = nelems / vec_sz;
-    unsigned nres = nelems % vec_sz;
+    auto nvec = nelems / vec_sz;
+    auto nres = nelems % vec_sz;
     for (unsigned vec = 0; vec < nvec; vec ++, src+=vec_sz, dst+=vec_sz) {
       __m256 vmm1 = _mm256_load_ps(src);
       __m256 vmm2 = _mm256_load_ps(dst);
@@ -86,6 +96,13 @@ public:
       vmm2 = op_mask(vmm1, vmm2, mask);
       _mm256_maskstore_ps(dst, mask, vmm2);
     }
+  }
+
+  template<typename vec_op, typename vec_op_mask, typename T = float>
+  static inline void vecwise_binary_op (T *dst, const T *src, size_t nelems,
+      vec_op op, vec_op_mask op_mask) {
+    if (nelems < thread_hold)
+      single_thread_vecwise_binary_op(dst, src, nelems, op, op_mask);
   }
 
   template<class elem_t = float>
@@ -110,8 +127,7 @@ public:
           vmm1 = _mm256_div_ps(ones, vmm1);
           return vmm1;
         };
-        single_thread_vecwise_unary_op(dst, src, nelems,
-            vec_inv_square, mask_vec_inv_square);
+        vecwise_unary_op(dst, src, nelems, vec_inv_square, mask_vec_inv_square);
       } else {
         throw error(mkldnn_unimplemented, "Not implemented!");
       }

cf88afc027cd8fe8c98ecd3fd3df332cacfb961b
lhz <hongzhen.liu@intel.com>
2018-04-03 19:39:50 +0800

    delete needless files
    Signed-off-by: lhz <hongzhen.liu@intel.com>

--- python/ideep4py/include/utils.h
+++ python/ideep4py/include/utils.h
@@ -29,45 +29,9 @@
 #include <iostream>
 #include <sstream>
 #include <cassert>
-#include "logging.h"
 #include "param.h"
 #include "omp.h"
 using namespace mkldnn;
-
-memory::format get_desired_format(int channel);
-memory::format get_desired_format_weight(int channel0, int channel1);
-template<typename T>
-void eltwise_multiply(T* x1, T* x2, T* y, size_t n) {
-#pragma omp parallel for schedule(static)
-    for (size_t i = 0; i < n; ++i) {
-        y[i] = x1[i] * x2[i];
-    }
-}
-
-//
-//// map C type with mkldnn's
-//// float -> memory::data_type::f32
-//// int -> memory::data_type::s32
-//// int16_t -> memory::data_type::s16
-//// int8_t -> memory::data_type::s8
-//// uint8_t -> memory::data_type::u8
-//
-template<typename T>
-static inline mkldnn::memory::data_type memory_data_type() {
-    if (typeid(T) == typeid(float))
-        return mkldnn::memory::data_type::f32;
-    else if (typeid(T) == typeid(int))
-        return mkldnn::memory::data_type::s32;
-    else if (typeid(T) == typeid(int16_t))
-        return mkldnn::memory::data_type::s16;
-    else if (typeid(T) == typeid(int8_t))
-        return mkldnn::memory::data_type::s8;
-    else if (typeid(T) == typeid(uint8_t))
-        return mkldnn::memory::data_type::u8;
-
-    LOG(ERROR) << "Not support type";
-    return mkldnn::memory::data_type::data_undef;
-}
 
 // utils function conver int/double/bool/dims/ to string
 static inline std::string int_to_string(int value) {
@@ -120,7 +84,6 @@ static inline mkldnn::algorithm pooling_algo_convert(pooling_param_t::algorithm 
         case pooling_param_t::algorithm::pooling_avg_exclude_padding:
             return mkldnn::pooling_avg_exclude_padding;
         default:
-            LOG(ERROR) << "Not a valid pooling algo";
             return mkldnn::pooling_max;
     }
 }
@@ -132,7 +95,6 @@ static inline mkldnn::algorithm lrn_algo_convert(lrn_param_t::algorithm input) {
         case lrn_param_t::algorithm::lrn_within_channel:
             return mkldnn::lrn_within_channel;
         default:
-            LOG(ERROR) << "Not a valid lrn algo";
             return mkldnn::lrn_across_channels;
     }
 }

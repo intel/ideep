e61be139b6268bee52576a716b62b0c28b84a12b
lhz <hongzhen.liu@intel.com>
2018-06-15 10:09:14 +0800

    fix build pypi package issue

--- python/setup.py
+++ python/setup.py
@@ -62,6 +62,12 @@ def clean_ext():
 ###############################################################################
 # Custom build commands
 ###############################################################################
+class build(distutils.command.build.build):
+    def run(self):
+        prepare_ext()
+        distutils.command.build.build.run(self)
+
+
 class build_ext(setuptools.command.build_ext.build_ext):
     def run(self):
         prepare_ext()
@@ -84,6 +90,7 @@ class clean(distutils.command.clean.clean):
 
 
 cmdclass = {
+    'build': build,
     'build_ext': build_ext,
     'install': install,
     'clean': clean,
